8522df3b70e15315c50360b1707ac78d98d111c3
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-10-01 12:02:31 -0300

    usando sort() Java com reescrita equals(), compareTo, stringTo

---
+++ 3-ordenacao/CollectionJava/Aluno.java
@@ -0,0 +1,35 @@
+public class Aluno implements Comparable<Aluno> {
+    public int idade;
+    public String nome;
+
+    public Aluno(int idade, String nome) {
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    @Override
+     public boolean equals(Object o) {
+        if (o instanceof Aluno) {
+            Aluno e = (Aluno) o;
+            return this.idade == e.idade && this.nome.equals(e.nome);
+        }
+        return false;
+    }
+    
+    @Override
+    public int compareTo(Aluno a) {
+        if (this.idade < a.idade) {
+            return -1;
+        } if (this.idade == a.idade) {
+            if (this.nome.compareTo(a.nome) < 0) {
+                 return -1;
+            }
+        }
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: "+ this.nome + ". Idade: " + this.idade; 
+    }
+}
---
+++ 3-ordenacao/CollectionJava/Principal.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String a[]) {
+        ArrayList<Aluno> listaAlunos = new ArrayList<>();
+
+        listaAlunos.add(new Aluno(46,"Alexandre Zamberlan"));
+        listaAlunos.add(new Aluno(20,"Alexandre Zeni"));
+        listaAlunos.add(new Aluno(19,"Meani"));
+        listaAlunos.add(new Aluno(27,"Ana"));
+        listaAlunos.add(new Aluno(5,"Dante"));
+        listaAlunos.add(new Aluno(19,"Bia"));
+
+        Collections.sort(listaAlunos);
+
+        System.out.println("Lista de alunos ordenados por idade");
+        for (int i = 0; i < listaAlunos.size(); i++) {
+            System.out.println(listaAlunos.get(i));
+        }
+    }
+}
