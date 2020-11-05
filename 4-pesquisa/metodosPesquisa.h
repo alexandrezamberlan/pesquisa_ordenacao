4152123b946a5431a34bf4f5b821e38f9ce5b2c9
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-11-05 10:41:14 -0300

    metodo pesquisa sequencial em cpp

---
+++ 4-pesquisa/metodosPesquisa.h
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std; 
+
+long long int pesquisaSequencialRetornaPrimeiraOcorrencia(int numero, int *vetor, long long int n) {
+    long long int i;
+    
+    for (i = 0; i < n; i++) {
+        if (numero == vetor[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void pesquisaSequencialMostraTodasAsPosicoes(int numero, int *vetor, long long int n) {
+    long long int i;
+    bool achou = false;
+    for (i = 0; i < n; i++) {
+        if (numero == vetor[i]) {
+            printf("%lld\t", i);
+            achou = true;
+        }
+    }
+    if (!achou) {
+        printf(" não localizado");
+    }
+    printf("\n\n");
+}
+
+Celula *pesquisaSequencialRetornaTodasAsOcorrencias(int numero, int *vetor, long long int n) {
+    long long int i;
+    bool achou = false;
+    Celula *lista = NULL;
+    
+    for (i = 0; i < n; i++) {
+        if (numero == vetor[i]) {
+            lista = inserirDesordenadoLista(i, lista);
+            achou = true;
+        }
+    }
+    return lista;
+}
---
+++ 4-pesquisa/principal.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <time.h>
+#include "../3-ordenacao/CPP/utilidades.h"
+#include "../3-ordenacao/CPP/metodosOrdenacao.h"
+#include "metodosPesquisa.h"
+
+using namespace std;
+
+int main() {
+    clock_t tempoInicio, tempoFim;
+    long long int tamanho;
+
+    system("clear");//no windows é system("cls");
+
+    cout << "Tamanho do vetor ou lista: ";
+    cin >> tamanho;
+    
+    int numero;
+    int *vetorSequencial, *vetorBinario;
+    vetorSequencial = (int*)malloc(sizeof(int) * tamanho);
+    vetorBinario = (int*)malloc(sizeof(int) * tamanho);
+    
+    printf("Vetor para pesquisa sequencial... desordenado\n");
+    popula(vetorSequencial, tamanho);
+    exibe(vetorSequencial, tamanho);
+
+    printf("Informe um inteiro para pesquisa: ");
+    scanf("%d", &numero);
+
+    // printf("O %d encontra-se nas posições: ", numero);
+    // pesquisaSequencialMostraTodasAsPosicoes(numero, vetorSequencial, tamanho);
+    
+    Celula *ocorrencias = NULL;
+    ocorrencias = pesquisaSequencialRetornaTodasAsOcorrencias(numero, vetorSequencial, tamanho);
+    printf("O %d encontra-se nas posições: \n", numero);
+    exibirLista(ocorrencias);
+
+
+
+    // printf("Vetor para pesquisa binária... deve estar ordenado");
+    // popula(vetorBinario, tamanho);
+    // pente(vetorBinario, tamanho);
+    // exibe(vetorBinario, tamanho);
+
+
+    return 1;
+}
