ff67de54ef755d0cc592981ae4192ebccda27c6f
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-12-03 11:41:23 -0300

    hash com nomes

--- 5-hash/CPP/hashTable6.c
+++ 5-hash/CPP/hashTable6.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define TAMANHO_TABELA 10
 
 /*
  * estrutura que representa uma lista duplamente encadeada
- * dado guarda um numero inteiro qualquer
+ * dado guarda um nome qualquer
  * ant - guarda o endereco da celula anterior
  * prox - guarda o enedereco da proxima celula
  */
 typedef struct nodo {
-	int dado;
+	char dado[100];
 	struct nodo *ant, *prox;
 } CelulaD;
 
 /*
- * metodo que recebe um valor inteiro e um ponteiro para uma lista duplamente encadeada
+ * metodo que recebe um nome e um ponteiro para uma lista duplamente encadeada
  * o metodo insere ordenadamente o valor na lista e a retorna modificada
  */
-CelulaD *inserir(int valor, CelulaD *l) {
+CelulaD *inserir(char valor[], CelulaD *l) {
 	CelulaD *novo;
 	CelulaD *pAnterior;
 
 	novo = (CelulaD *)malloc(sizeof(CelulaD));
-	novo->dado = valor;
+	strcpy(novo->dado, valor);
 	novo->ant = NULL;
 	novo->prox = NULL;
 
@@ -32,12 +33,12 @@ CelulaD *inserir(int valor, CelulaD *l) {
 
 	pAnterior = NULL;
 	for (; l; l = l->prox) {
-		if (valor == l->dado) {
+		if (strcmp(valor, l->dado) == 0) {
 			printf("Dado jah cadastrado\n");
 			free(novo);
 			return l;
 		}
-		if (valor < l->dado) {
+		if (strcmp(valor, l->dado) < 0) {
 			//insercao aqui
 			if (!pAnterior) { //1o elemento
 				novo->prox = l;
@@ -55,14 +56,14 @@ CelulaD *inserir(int valor, CelulaD *l) {
 	return novo;
 }
 
-CelulaD *remover(int valor, CelulaD *l) {
+CelulaD *remover(char valor[], CelulaD *l) {
 	CelulaD *pAnterior, *lixo;
 
 	if (l) {
 		while (l->ant) l = l->ant;
 
 		for (pAnterior = NULL; l; l = l->prox) {
-			if (valor == l->dado) { //achei
+			if (strcmp(valor, l->dado) == 0) { //achei
 				if (!pAnterior) { //1o elemento
 					lixo = l;
 					l = l->prox;
@@ -95,7 +96,7 @@ void exibir(CelulaD *l) {
 		while (l->ant) l = l->ant;
 
 		for (; l; l = l->prox) {
-			printf("%d\t", l->dado);
+			printf("%s\t", l->dado);
 		}
 		printf("\n");
 	}
@@ -116,52 +117,44 @@ void inicializaTabelaHash(CelulaD *tabela[], int n) {
 /*
  * funcao que recebe a chave de um dado e retorne sua posicao na tabela hash
  */
-int funcaoHash(int chave) {
-	return chave % TAMANHO_TABELA;
+int funcaoHash(char chave[]) {
+	int i;
+	int soma = 0;
+	//alex -> 97+108+101+120 -> soma
+	for (i = 0; i < strlen(chave); i++) {
+		soma = soma + (int)chave[i];
+	}
+	return soma % TAMANHO_TABELA;
+}
+
+void adicionar(char chave[], CelulaD *tabela[]){
+	int endereco = funcaoHash(chave);
+	
+	tabela[endereco] = inserir(chave, tabela[endereco]);
+}
+
+void exibirTabelaHash(CelulaD *tabela[], int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		printf("Endereco %d: ", i);
+		exibir(tabela[i]);
+	}
 }
 
 int main() {
 	CelulaD *tabelaHash[TAMANHO_TABELA];
-	int dado;
-	int endereco;
 	int i;
 
 	inicializaTabelaHash(tabelaHash, TAMANHO_TABELA);
+	
+	adicionar("alexandre", tabelaHash);
+	adicionar("pedro", tabelaHash);
+	adicionar("alex", tabelaHash);
+	adicionar("zeni", tabelaHash);
+	adicionar("augusto", tabelaHash);
+	adicionar("alex", tabelaHash);
 
-	int opcao;
-
-	do {
-		printf("1 - Inserir; 2 - Remover; 3 - Exibir; 4 - Sair: ");
-		scanf("%d", &opcao);
-		switch (opcao) {
-			case 1 : //inserir
-			         printf("\nDigite um numero inteiro qualquer: ");
-			         scanf("%d", &dado);
-			         endereco = abs(funcaoHash(dado));
-			         printf("%d sera inserido na posicao %d da tabela\n", dado, endereco);
-			         if (tabelaHash[endereco]) printf("Dado em colisao!\n");
-			         tabelaHash[endereco] = inserir(dado, tabelaHash[endereco]);
-			         break;
-			case 2 : //remover
-				     printf("\nDigite um numero inteiro qualquer para remocao: ");
-			         scanf("%d", &dado);
-			         endereco = abs(funcaoHash(dado));
-			         if (tabelaHash[endereco]) {
-			         	printf("%d sera removido na posicao %d da tabela\n", dado, endereco);
-			         	tabelaHash[endereco] = remover(dado, tabelaHash[endereco]);
-			         } else printf("Numero nao localizado. Tabela vazia!\n");;
-			         break;
-			case 3 : //exibir
-					 for (i = 0; i < TAMANHO_TABELA; i++) {
-					 	printf("Valores da tabela na posicao %d: ", i);
-					 	exibir(tabelaHash[i]);
-					 }
-			         break;
-			case 4 : //vai embora
-			         break;
-			default : printf("Opcao invalida!\n");
-		}
-	} while (opcao != 4);
+	exibirTabelaHash(tabelaHash, TAMANHO_TABELA);
 
 	return 1;
 }
