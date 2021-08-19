cbb53679c0d6fd0248363a03faacd3b6ac46c03e
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2021-08-19 11:33:49 -0300

    manipulando arquivos

---
+++ 2-manipulaArquivos/CPP/arquivo1_lendoDadosArquivo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    //abrir um arquivo texto e exibir seu conteudo na tela
+
+    //solicita pro usuario nome do arquivo origem
+	char nomeArquivo[200];
+	cout << "Informe nome do arquivo que quer exibir na tela: ";
+	cin >> nomeArquivo;
+    
+    //abrir arquivo para leitura
+	//https://www.tutorialspoint.com/cplusplus/cpp_files_streams
+	ifstream procuradorArquivo; 
+    procuradorArquivo.open(nomeArquivo); 
+ 
+   		
+	//le o arquivo capturando as frases
+	string frase;
+	while (procuradorArquivo >> frase) {
+		cout << frase << endl;
+	}
+	procuradorArquivo.close();
+
+    return 1;
+}
