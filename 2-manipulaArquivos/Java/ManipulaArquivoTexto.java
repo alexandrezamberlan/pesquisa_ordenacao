a8f5f4a132edc9f6280e8dab088fa6707a53d6c0
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2021-08-26 10:14:34 -0300

    tarefa de arquivo em Java por Pedro

---
+++ 2-manipulaArquivos/Java/ManipulaArquivoTexto.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import javax.swing.JOptionPane;
+
+public class ManipulaArquivoTexto {
+
+    public static void gravarNoArquivo() {
+        String nomeArquivo, frase;
+        try {
+            FileWriter fw;
+            //fw = new FileWriter("nomesjava.txt"); // Reescreve o arquivo arq.txt
+            nomeArquivo = JOptionPane.showInputDialog(null, "Nome do arquivo");
+            fw = new FileWriter(nomeArquivo, true); // Não reescreve o arquivo arq.txt (adiciona ao arquivo)
+            do {
+                frase = JOptionPane.showInputDialog(null, "Digite o que deseja inserir no arquivo ou a palavra 'fim' para encerrar: ");
+                if (frase.equalsIgnoreCase("fim")){  // ignora se o caractere é maiúsculo ou minúsculo ("fim" ou "FIM")
+                    break;
+                }
+                fw.write(frase + "\n"); // Escreve a string na linha    
+            } while(true);
+            fw.close();
+        }catch (Exception e){
+            System.out.println("Erro "+ e.getMessage());
+        }
+    }
+
+    public static void lerArquivo() {
+        String nomeArquivo;
+        try {
+            nomeArquivo = JOptionPane.showInputDialog(null, "Nome do arquivo");
+            FileReader fr;
+            fr = new FileReader(nomeArquivo);
+            BufferedReader bf = new BufferedReader(fr);
+            String linha;
+            do {
+                linha = bf.readLine();
+                if (linha == null) {
+                    break;
+                }
+                System.out.println(linha);
+            } while(true);
+            bf.close();
+        }catch (Exception e){
+            System.out.println("Erro "+ e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        gravarNoArquivo();
+        lerArquivo();
+    }
+}
