4896ab8b5b19b199687051e2249ce3310d34516a
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2021-08-26 11:59:29 -0300

    bolha no C#

--- 3-ordenacao/Java/Principal.java
+++ 3-ordenacao/Java/Principal.java
@@ -5,26 +5,14 @@ import java.util.Collections;
 
 public class Principal {
 
-    public static void popular(int quantidade, ArrayList<Integer> lista) {
-        Random gerador = new Random();
-        for (int i = 0; i < quantidade; i++) {
-            lista.add(gerador.nextInt(100));
-        }
-    }
-
-    public static void exibir(ArrayList<Integer> lista) {
-        for (int i = 0; i < lista.size(); i++) {
-            System.out.println(lista.get(i));
-        }
-    }
     public static void main(String a[]) {
         ArrayList<Integer> listaBolha = new ArrayList<Integer>();
         long tempoInicio, tempoFim;  
         
         int quantidade = Integer.parseInt(JOptionPane.showInputDialog(null,"Quantos números quer gerar? "));
 
-        popular(quantidade, listaBolha);
-        //exibir(listaBolha);
+        Util.popular(quantidade, listaBolha);
+        //Util.exibir(listaBolha);
 
         //bolha
         tempoInicio = System.nanoTime();
---
+++ 3-ordenacao/Java/Util.java
@@ -0,0 +1,14 @@
+public class Util {
+    public static void popular(int quantidade, ArrayList<Integer> lista) {
+        Random gerador = new Random();
+        for (int i = 0; i < quantidade; i++) {
+            lista.add(gerador.nextInt(100));
+        }
+    }
+
+    public static void exibir(ArrayList<Integer> lista) {
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+        }
+    }
+}
