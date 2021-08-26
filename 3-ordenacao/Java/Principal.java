dfa5f37a13b54f9e64b30114eae3ccad4600a06a
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2021-08-26 11:29:29 -0300

    bolha no java

--- 3-ordenacao/Java/Principal.java
+++ 3-ordenacao/Java/Principal.java
@@ -18,83 +18,20 @@ public class Principal {
         }
     }
     public static void main(String a[]) {
-        // ArrayList<Integer> listaBolha = new ArrayList<Integer>();
-        // ArrayList<Integer> listaSelecao = new ArrayList<Integer>();
-        // ArrayList<Integer> listaInsercao = new ArrayList<Integer>();
-        // ArrayList<Integer> listaAgitacao = new ArrayList<Integer>();
-        ArrayList<Integer> listaPente = new ArrayList<Integer>();
-        ArrayList<Integer> listaShell = new ArrayList<Integer>();
-        ArrayList<Integer> listaQuick = new ArrayList<Integer>();
-        ArrayList<Integer> listaHeap = new ArrayList<Integer>();
-        ArrayList<Integer> lista = new ArrayList<Integer>();
-        long tempoInicio;  
+        ArrayList<Integer> listaBolha = new ArrayList<Integer>();
+        long tempoInicio, tempoFim;  
         
         int quantidade = Integer.parseInt(JOptionPane.showInputDialog(null,"Quantos números quer gerar? "));
 
-        // popular(quantidade, listaBolha);
-        // popular(quantidade, listaSelecao);
-        // popular(quantidade, listaInsercao);
-        // popular(quantidade, listaAgitacao);
-        popular(quantidade, listaPente);
-        popular(quantidade, listaShell);
-        popular(quantidade, listaQuick);
+        popular(quantidade, listaBolha);
+        //exibir(listaBolha);
 
-        popular(quantidade, listaHeap); 
-        listaHeap.add(23); //adicionado mais um numero no final da listaHeap
-        listaHeap.set(0, -99); //posição 0 com lixo
-        
-        popular(quantidade, lista);
-        System.out.println("Total de números: " + quantidade);
-
-        // //bolha
-        // tempoInicio = System.nanoTime();
-        // MetodosOrdenacao.bolha(listaBolha);
-        // System.out.println("Tempo (ms) bolha: " + (System.nanoTime() - tempoInicio)/1000000);
-        
-        // //selecao
-        // tempoInicio = System.nanoTime();
-        // MetodosOrdenacao.selecao(listaSelecao);
-        // System.out.println("Tempo (ms) seleção: " + (System.nanoTime() - tempoInicio)/1000000);
-        
-        // //insercao
-        // tempoInicio = System.nanoTime();
-        // MetodosOrdenacao.insercao(listaInsercao);
-        // System.out.println("Tempo (ms) inserção: " + (System.nanoTime() - tempoInicio)/1000000);
-
-        // //agitacao
-        // tempoInicio = System.nanoTime();
-        // MetodosOrdenacao.agitacao(listaAgitacao);
-        // System.out.println("Tempo (ms) agitação: " + (System.nanoTime() - tempoInicio)/1000000);
-        
-        //pente
+        //bolha
         tempoInicio = System.nanoTime();
-        MetodosOrdenacao.pente(listaPente);
-        System.out.println("Tempo (ms) pente: " + (System.nanoTime() - tempoInicio)/1000000);
-        //exibir(listaPente);
-
-         //shell
-         tempoInicio = System.nanoTime();
-         MetodosOrdenacao.shell(listaShell);
-         System.out.println("Tempo (ms) shell: " + (System.nanoTime() - tempoInicio)/1000000);
-         //exibir(listaShell);
-
-        //quick
-        tempoInicio = System.nanoTime();
-        MetodosOrdenacao.quickSort(listaQuick, 0, listaQuick.size() - 1);
-        System.out.println("Tempo (ms) quick: " + (System.nanoTime() - tempoInicio)/1000000);
-        //exibir(listaQuick);
-        
-        //heap
-        tempoInicio = System.nanoTime();
-        MetodosOrdenacao.heapSort(listaHeap);
-        System.out.println("Tempo (ms) heap: " + (System.nanoTime() - tempoInicio)/1000000);
-        // exibir(listaHeap);
- 
-        //usando o próprio Java para ordenar
-        tempoInicio = System.nanoTime();
-        Collections.sort(lista);
-        System.out.println("Tempo (ms) do método do Java: " + (System.nanoTime() - tempoInicio)/1000000);
-        
+        MetodosOrdenacao.bolha(listaBolha);
+        tempoFim = System.nanoTime();
+        System.out.println("Tempo (ms) bolha: " + (tempoFim - tempoInicio)/1000000);
+               
         System.exit(0);
     }
 }
