dc91fab3082963582d9984484b297c37689be87e
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2021-08-26 11:55:20 -0300

    bolha no C#

--- 3-ordenacao/C#/Program.cs
+++ 3-ordenacao/C#/Program.cs
@@ -8,59 +8,19 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            //List<int> listaBolha = new List<int>();
-            //List<int> listaSelecao = new List<int>();
-            //List<int> listaInsercao = new List<int>();
-            //List<int> listaAgitacao = new List<int>();
-            List<int> listaPente = new List<int>();
-            List<int> listaShell = new List<int>();
-
+            List<int> listaBolha = new List<int>();
+            
             Console.Write("Quantidade números: ");
             int qtd = Int32.Parse(Console.ReadLine());
 
-            //Utilidades.popularLista(listaBolha, qtd);
-            //Utilidades.popularLista(listaSelecao, qtd);
-            //Utilidades.popularLista(listaInsercao, qtd);
-            //Utilidades.popularLista(listaAgitacao, qtd);
-            Utilidades.popularLista(listaPente, qtd);
-            Utilidades.popularLista(listaShell, qtd);
-
+            Utilidades.popularLista(listaBolha, qtd);
+            
             Stopwatch sw = new Stopwatch();
 
-            //sw.Start();
-            //Ordenacao.bolha(listaBolha);
-            //sw.Stop();
-            //Console.WriteLine("Fim do bolha (ms): " + sw.ElapsedMilliseconds);
-            //sw.Reset();
-
-            //sw.Start();
-            //Ordenacao.selecao(listaSelecao);
-            //sw.Stop();
-            //Console.WriteLine("Fim do selecao (ms): " + sw.ElapsedMilliseconds);
-            //sw.Reset();
-
-            //sw.Start();
-            //Ordenacao.insercao(listaInsercao);
-            //sw.Stop();
-            //Console.WriteLine("Fim do insercao (ms): " + sw.ElapsedMilliseconds);
-            //sw.Reset();
-
-            //sw.Start();
-            //Ordenacao.agitacao(listaAgitacao);
-            //sw.Stop();
-            //Console.WriteLine("Fim do agitação (ms): " + sw.ElapsedMilliseconds);
-            //sw.Reset();
-
             sw.Start();
-            Ordenacao.pente(listaPente);
+            Ordenacao.bolha(listaBolha);
             sw.Stop();
-            Console.WriteLine("Fim do pente (ms): " + sw.ElapsedMilliseconds);
-            sw.Reset();
-
-            sw.Start();
-            Ordenacao.shell(listaShell);
-            sw.Stop();
-            Console.WriteLine("Fim do shell (ms): " + sw.ElapsedMilliseconds);
+            Console.WriteLine("Fim do bolha (ms): " + sw.ElapsedMilliseconds);
             sw.Reset();
         }
     }
