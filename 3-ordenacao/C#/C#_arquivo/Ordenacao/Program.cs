9086c62f595cc2d6bf9f18df1e297347ccf81211
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2022-08-25 10:23:04 -0300

    Add files via upload

---
+++ 3-ordenacao/C#/C#_arquivo/Ordenacao/Program.cs
@@ -0,0 +1,57 @@
+﻿using Ordenacao;
+using System.Diagnostics;
+
+List<int> listaSelecao = new List<int>();
+List<int> listaInsercao = new List<int>();
+List<int> listaBolha = new List<int>();
+List<int> listaAgitacao = new List<int>();
+List<int> listaPente = new List<int>();
+
+
+string nomeArquivo;
+Console.WriteLine("Informe caminho e nome de arquivo a ser utilizado: ");
+nomeArquivo = Console.ReadLine();
+
+Util.popularDoArquivo(nomeArquivo, listaSelecao);
+//Util.exibirLista("Selecao",listaSelecao);
+listaInsercao.AddRange(listaSelecao);
+listaBolha.AddRange(listaSelecao);
+listaAgitacao.AddRange(listaSelecao);
+listaPente.AddRange(listaSelecao);
+
+
+Stopwatch sw = new Stopwatch();
+
+sw.Start();
+Ordena.bolha(listaBolha);
+sw.Stop();
+Console.WriteLine("Fim do bolha (ms): " + sw.ElapsedMilliseconds);
+sw.Reset();
+
+sw.Start();
+Ordena.selecao(listaSelecao);
+sw.Stop();
+Console.WriteLine("Fim do seleção (ms): " + sw.ElapsedMilliseconds);
+sw.Reset();
+
+sw.Start();
+Ordena.insercao(listaInsercao);
+sw.Stop();
+Console.WriteLine("Fim do insercao (ms): " + sw.ElapsedMilliseconds);
+sw.Reset();
+
+sw.Start();
+Ordena.agitacao(listaAgitacao);
+sw.Stop();
+Console.WriteLine("Fim do agitacao (ms): " + sw.ElapsedMilliseconds);
+sw.Reset();
+
+sw.Start();
+Ordena.pente(listaPente);
+sw.Stop();
+Console.WriteLine("Fim do pente (ms): " + sw.ElapsedMilliseconds);
+sw.Reset();
+
+
+
+
---
+++ 3-ordenacao/C#/C#_arquivo/Ordenacao/Util.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenacao
+{
+    public class Util
+    {
+        /// <summary>
+        /// método static que abre um arquivo plain text e coloca seus valores em uma lista
+        /// </summary>
+        /// <param name="nomeArquivo">arquivo contendo números inteiros um abaixo do outro</param>
+        /// <param name="lista">lista que receberá os números inteiros vindos do arquivo</param>
+        public static void popularDoArquivo(string nomeArquivo, List<int> lista)
+        {
+            StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8); //abre o arquivo para leitura
+            int numero;
+            do
+            {
+                numero = int.Parse(leitor.ReadLine());
+                lista.Add(numero);
+            } while (!leitor.EndOfStream);
+            leitor.Close(); //fecha o objeto que representa o arquivo
+        }
+
+        public static void exibirLista(string frase, List<int> lista)
+        {
+            Console.WriteLine(frase);
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
