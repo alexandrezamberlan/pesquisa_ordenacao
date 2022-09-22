ec77ae0502e7eadb9e7e6f4a3f3f0336d5c83552
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-09-22 11:16:47 -0300

    usando o sort em c# com definição de chave de ordenação

---
+++ 3-ordenacao/ColecaoCSharp/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColecaoCSharp
+{
+    public class Aluno : IComparable<Aluno>
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Aluno(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public int CompareTo(Aluno? other)
+        {
+            if (this.Id < other.Id)
+                return -1;
+            if (this.Id == other.Id)
+            {
+                if (this.Nome.CompareTo(other.Nome) < 0)
+                    return -1;
+            }
+            return 1;
+        }
+    }
+}
---
+++ 3-ordenacao/ColecaoCSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using ColecaoCSharp;
+
+List<Aluno> lista = new List<Aluno>();
+
+lista.Add(new Aluno(45,"Alexandre"));
+lista.Add(new Aluno(23, "Pedro"));
+lista.Add(new Aluno(19, "Matheus"));
+lista.Add(new Aluno(19, "Gabriel"));
+
+lista.Sort();
+
+//lista.Sort((a, b) => a.Id.CompareTo(b.Id));
+
+foreach (var item in lista)
+{
+    Console.WriteLine(item.Id + "  " + item.Nome);
+}
+
+
+
+//SortedList<int, string> numberNames = new SortedList<int, string>();
+//numberNames.Add(3, "Three");
+//numberNames.Add(1, "One");
+//numberNames.Add(2, "Two");
+//numberNames.Add(4, null);
+//numberNames.Add(10, "Ten");
+//numberNames.Add(5, "Five");
+
+//foreach (var item in numberNames)
+//{
+//    Console.WriteLine(item);
+//}
