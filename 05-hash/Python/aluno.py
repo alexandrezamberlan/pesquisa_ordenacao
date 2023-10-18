cc2e3f603023c2f45ed6b0039fa35720a4ec4072
Alexandre <alexz@ufn.edu.br>
2023-10-18 09:02:03 -0300

    hash em python

---
+++ 05-hash/Python/aluno.py
@@ -0,0 +1,21 @@
+class Aluno:
+    def __init__(self, matricula, nome):
+        self.matricula = matricula
+        self.nome = nome
+        
+
+    def __eq__(self, other):
+        if isinstance(other, Aluno):
+            return self.matricula == other.matricula
+        return False
+    
+    def __gt__(self, other):
+        if self.matricula == other.matricula:
+            return self.nome > other.nome
+        return self.matricula > other.matricula
+    
+    def __str__(self):
+        return "Aluno{" + "matricula=" + str(self.matricula) + ", nome=" + self.nome + '}'
+    
+    def __hash__(self):
+        return hash((self.matricula, self.nome))
