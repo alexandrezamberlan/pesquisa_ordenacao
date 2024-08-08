4c5b3a336b6ec54b95bfbf3dad8eb45934b260c6
Alexandre de O. Zamberlan <alexz@ufn.edu.br>
2024-08-08 18:59:01 -0300

    Add files via upload

--- 01-revisao/revisaoPython/principal.py
+++ 01-revisao/revisaoPython/principal.py
@@ -1,8 +1,15 @@
 from util import Util
 import random
 #gerar n numeros de range m
+# lista_numeros = []
+# Util.gerar_numeros_lista(lista_numeros, 100, 500)
+# Util.exibir(lista_numeros,'Lista de numeros')
 
 #gerar n palavras de tamanho m
-lista_palavras = []
-Util.gerar_palavras_lista(lista_palavras, 100, 5)
-Util.exibir(lista_palavras,'Lista de palavras')
+# lista_palavras = []
+# Util.gerar_palavras_lista(lista_palavras, 100, 11)
+# Util.exibir(lista_palavras,'Lista de palavras')
+
+#gerar n palavras de tamanho m em arquivo
+nome_arquivo = 'palavras.txt'
+Util.gerar_palavras_arquivo(nome_arquivo, 100, 11)
--- 01-revisao/revisaoPython/util.py
+++ 01-revisao/revisaoPython/util.py
@@ -3,6 +3,28 @@ import random
 class Util:
     """ Classe com métodos estáticos para gerar números e/ou palavras aleatórios
     """
+
+    @staticmethod
+    def gerar_palavras_arquivo(nome_arquivo, quantidade, tamanho):
+        # Abrindo o arquivo em modo de escrita ('w'). 
+        # Isso cria o arquivo se ele não existir ou o sobrescreve se ele já existir.
+        with open(nome_arquivo, 'w', encoding='utf-8') as arquivo:
+            for i in range(0,quantidade):
+                arquivo.write( Util.gerar_palavra(tamanho) + '\n' )            
+
+        print(f"As palavras foram salvas em {nome_arquivo}.")
+
+    @staticmethod
+    def gerar_numeros_lista(lista, quantidade, tamanho):
+        """ Método estático que popula um lista com números inteiros aleatórios
+
+        Args:
+            lista (int): contém os números gerados
+            quantidade (int): quantos números se deseja gerar
+            tamanho (int): quantidade de dígitos do número a ser gerado
+        """
+        for i in range(0,quantidade):
+            lista.append( random.randint(0,tamanho) )
      
     @staticmethod
     def gerar_palavra(tamanho):
