fe579fb4dfcb4263c664226d99de215f5479891e
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2025-08-21 19:59:43 -0300

    aula 4 com geração de grafico por matplotlib

--- 03-ordenacao/Python/principal.py
+++ 03-ordenacao/Python/principal.py
@@ -1,28 +1,34 @@
+import matplotlib.pyplot as plt
+import time
+
 from util import Util
 from ordenacao import Ordenacao
-import time
 
 lista_bolha = []
 lista_normal = []
 lista_selecao = []
 lista_insercao = []
 
-Util.popular_lista_aleatoria(lista_bolha, 10000, 100, 20000)
-Util.popular_lista_aleatoria(lista_normal, 10000, 100, 20000)
-Util.popular_lista_aleatoria(lista_selecao, 10000, 100, 20000)
-Util.popular_lista_aleatoria(lista_insercao, 10000, 100, 20000)
+tamanho = 10000
+
+Util.popular_lista_aleatoria(lista_bolha, tamanho, 100, 20000)
+Util.popular_lista_aleatoria(lista_normal, tamanho, 100, 20000)
+Util.popular_lista_aleatoria(lista_selecao, tamanho, 100, 20000)
+Util.popular_lista_aleatoria(lista_insercao, tamanho, 100, 20000)
 
 #sort
 tempoInicio = time.perf_counter()
 lista_normal.sort()
 tempoFim = time.perf_counter()
-print("Tempo da rotina ordenar por sort nativo: ", (tempoFim - tempoInicio) , "s")        
+tempo_sort = tempoFim - tempoInicio
+print("Tempo da rotina ordenar por sort nativo: ",  tempo_sort, "s")        
 
 #bolha
 tempoInicio = time.perf_counter()
 qtd_comparacoes, qtd_trocas = Ordenacao.bolha(lista_bolha)
 tempoFim = time.perf_counter()
-print("Tempo da rotina ordenar por bolha: ", (tempoFim - tempoInicio) , "s")      
+tempo_bolha = tempoFim - tempoInicio
+print("Tempo da rotina ordenar por bolha: ", tempo_bolha , "s")      
 print('Comparacoes:', qtd_comparacoes)
 print('Trocas:', qtd_trocas)  
 
@@ -30,7 +36,8 @@ print('Trocas:', qtd_trocas)
 tempoInicio = time.perf_counter()
 qtd_comparacoes, qtd_trocas = Ordenacao.selecao(lista_selecao)
 tempoFim = time.perf_counter()
-print("Tempo da rotina ordenar por selecao: ", (tempoFim - tempoInicio) , "s")      
+tempo_selecao = tempoFim - tempoInicio
+print("Tempo da rotina ordenar por selecao: ", tempo_selecao , "s")      
 print('Comparacoes:', qtd_comparacoes)
 print('Trocas:', qtd_trocas)  
 
@@ -38,6 +45,22 @@ print('Trocas:', qtd_trocas)
 tempoInicio = time.perf_counter()
 qtd_comparacoes, qtd_trocas = Ordenacao.insercao(lista_insercao)
 tempoFim = time.perf_counter()
-print("Tempo da rotina ordenar por insercao: ", (tempoFim - tempoInicio) , "s")      
+tempo_insercao = tempoFim - tempoInicio
+print("Tempo da rotina ordenar por insercao: ", tempo_insercao , "s")      
 print('Comparacoes:', qtd_comparacoes)
 print('Trocas:', qtd_trocas)  
+
+
+plt.figure(figsize=(10, 7))
+plt.plot(tempo_sort, tamanho, marker="o", linestyle="-", label=f"sort")
+plt.plot(tempo_bolha, tamanho,marker="o", linestyle="-", label=f"bolha")
+plt.plot(tempo_selecao, tamanho,marker="o", linestyle="-", label=f"selecao")
+plt.plot(tempo_insercao, tamanho,marker="o", linestyle="-", label=f"insercao")
+
+
+plt.title(f"Comparação de Algoritmos de Ordenação\n(Entrada: Listas desordenadas e aleatorias)")
+plt.xlabel("Tempo de Execução (segundos)")
+plt.ylabel("Tamanho da Lista (n)")
+plt.legend()
+plt.grid(axis='y', linestyle=':')
+plt.show()
